bd389784488ae91c959527f66f0b35c3f24cf3c1
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2023-08-17 16:09:33 +1200

    PATCH: excluding ones already done

--- _config.php
+++ _config.php
@@ -1,5 +1,6 @@
 <?php
 
+use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\Core\ClassInfo;
 use SilverStripe\ORM\DataObject;
 use Wilr\GoogleSitemaps\Extensions\GoogleSitemapExtension;
@@ -11,6 +12,8 @@ if (0 === strpos(ltrim($_SERVER['REQUEST_URI'], '/'), 'sitemap')) {
         GoogleSitemap::register_dataobject($className, 'daily');
     }
     foreach(ClassInfo::classesWithExtension(GoogleSitemapExtension::class, DataObject::class, false) as $className) {
-        GoogleSitemap::register_dataobject($className, 'daily');
+        if(!is_subclass_of($className, SiteTree::class)) {
+            GoogleSitemap::register_dataobject($className, 'daily');
+        }
     }
 }
